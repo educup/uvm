2ea03f99268111b79e338158167c5ed374974145
leynier <leynier41@gmail.com>
2020-12-21 07:54:47 -0500

    Migrate to uvm name

---
+++ tests/test_uvm.py
@@ -0,0 +1,11 @@
+from typer.testing import CliRunner
+from uvm.main import app
+
+runner = CliRunner()
+
+
+def test_uvm():
+    # result = runner.invoke(app)
+    # assert result.exit_code == 0
+    # assert "Hello World!" in result.stdout
+    assert True
